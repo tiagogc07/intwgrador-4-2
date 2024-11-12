222173f35af1b4528a16af38c6f5e4572391ce93
tiagogc07 <tmazuzzo@gmail.com>
2024-11-12 11:24:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const preguntas = [
+    { pregunta: "¿Cuál es la capital de Francia?", opciones: ["Madrid", "París", "Berlín"], respuesta: "París" },
+    { pregunta: "¿En qué año llegó el hombre a la Luna?", opciones: ["1969", "1959", "1979"], respuesta: "1969" },
+    { pregunta: "¿Quién escribió 'Don Quijote de la Mancha'?", opciones: ["Lope de Vega", "Miguel de Cervantes", "Pedro Calderón"], respuesta: "Miguel de Cervantes" },
+    { pregunta: "¿Cuántos continentes existen?", opciones: ["6", "7", "8"], respuesta: "7" },
+    { pregunta: "¿Qué es la fotosíntesis?", opciones: ["Proceso de la digestión", "Proceso de obtención de energía en las plantas", "Proceso de respiración celular"], respuesta: "Proceso de obtención de energía en las plantas" },
+    { pregunta: "¿Quién pintó la Mona Lisa?", opciones: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci"], respuesta: "Leonardo da Vinci" },
+    { pregunta: "¿Cuál es el río más largo del mundo?", opciones: ["Amazonas", "Nilo", "Yangtsé"], respuesta: "Amazonas" },
+    { pregunta: "¿Cuál es el idioma más hablado del mundo?", opciones: ["Español", "Chino mandarín", "Inglés"], respuesta: "Chino mandarín" },
+    { pregunta: "¿En qué año terminó la Segunda Guerra Mundial?", opciones: ["1945", "1939", "1950"], respuesta: "1945" },
+    { pregunta: "¿Qué es la gravedad?", opciones: ["Fuerza que atrae los objetos hacia el centro de la Tierra", "Fuerza que mantiene los planetas en órbita", "Fuerza que afecta a los gases"], respuesta: "Fuerza que atrae los objetos hacia el centro de la Tierra" },
+];
+
+let respuestasCorrect
